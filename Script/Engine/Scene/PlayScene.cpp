66eac8b8bb69869750131ec80bb3c5d407088ba8
ha sugi <suginuma1@outlook.jp>
2026-05-31 18:07:32 +0900

    スクリーンの枠を描画

--- Script/Engine/Scene/PlayScene.cpp
+++ Script/Engine/Scene/PlayScene.cpp
@@ -34,7 +34,28 @@ void PlayScene::Update()
 
 void PlayScene::Draw()
 {
+	DrawScreenFrame();
+
 	DrawString(100, 0, "PlayScene", 0xffffff);
 	DrawString(100, 20, "Push [N]Key To Title", 0xffffff);
+}
+
+void PlayScene::DrawScreenFrame()
+{
+	Vector2 p1 = { 0,0 };
+	Vector2 p2 = { SCREEN_OFFSET_X,WIN_HEIGHT };
+	DrawBox((int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y, COL_BLACK, TRUE);
+
+	p1 = { SCREEN_OFFSET_X,0 };
+	p2 = { SCREEN_WIDTH + SCREEN_OFFSET_X,SCREEN_OFFSET_Y };
+	DrawBox((int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y, COL_BLACK, TRUE);
+
+	p1 = { SCREEN_OFFSET_X,SCREEN_HEIGHT + SCREEN_OFFSET_Y };
+	p2 = { SCREEN_WIDTH + SCREEN_OFFSET_X ,WIN_HEIGHT };
+	DrawBox((int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y, COL_BLACK, TRUE);
+
+	p1 = { SCREEN_WIDTH + SCREEN_OFFSET_X ,0 };
+	p2 = { SCREEN_WIDTH + SCREEN_OFFSET_X * 2, WIN_HEIGHT };
+	DrawBox((int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y, COL_BLACK, TRUE);
 }
 
